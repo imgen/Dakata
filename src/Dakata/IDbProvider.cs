6eec79239a2c94781808e63be180cd83af4d6639
Imgen <imgen@hotmail.com>
2018-07-28 22:54:48 +0800

    Remove unused code

--- src/Dakata/IDbProvider.cs
+++ src/Dakata/IDbProvider.cs
@@ -10,7 +10,6 @@ namespace Dakata
         DbEngines DbEngine { get; }
         IDbConnection CreateConnection(string connectionString);
         Func<Compiler> SqlCompilerProvider { get; }
-        string DbConnectionName { get; }
         string UtcNowExpression { get; }
         long Insert(string sql, object parameters, IDbConnection connection);
         int MaxParameterCount { get; }
