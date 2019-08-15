71ce50bca948714ea3caed36010858fc3c5c3db9
Imgen <imgen@hotmail.com>
2019-08-15 23:15:22 +0800

    Add example project

---
+++ src/Dakata.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dakata.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
