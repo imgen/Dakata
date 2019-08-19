1364e710398ebdc4349886147b04d679b6ad4e71
Imgen <imgen@hotmail.com>
2019-08-19 23:31:36 +0800

    Add Db Providers

---
+++ src/Dakata.MySql/MySqlDbProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Dapper;
+using MySql.Data.MySqlClient;
+using SqlKata.Compilers;
+
+namespace Dakata.MySql
+{
+    public class MySqlDbProvider : IDbProvider
+    {
+        public DbEngines DbEngine { get; } = DbEngines.MySql;
+
+        public IDbConnection CreateConnection(string connectionString)
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public Func<Compiler> SqlCompilerProvider => () => new MySqlCompiler();
+        public string DbConnectionName { get; } = "MySqlConnection";
+        public string UtcNowExpression { get; } = "NOW()";
+
+        public long Insert(string sql, object parameters, IDbConnection connection)
+        {
+            connection.ExecuteScalar<long>(sql, parameters);
+            var results = connection.Query<dynamic>("Select LAST_INSERT_ID() id");
+            dynamic first = results.FirstOrDefault();
+            if (first == null)
+            {
+                return 0;
+            }
+            var id = first.id;
+            return id == null ? 0 : (long)id;
+        }
+
+        public int MaxParameterCount { get; } = 1000;
+    }
+}
---
+++ src/Dakata.SqlServer/SqlServerDbProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using SqlKata.Compilers;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+
+namespace Dakata.Example
+{
+    public class SqlServerDbProvider : IDbProvider
+    {
+        public DbEngines DbEngine => DbEngines.SqlServer;
+
+        public Func<Compiler> SqlCompilerProvider => () => new SqlServerCompiler();
+
+        public string UtcNowExpression => "SYSUTCDATETIME()";
+
+        public int MaxParameterCount => 2100;
+
+        public IDbConnection CreateConnection(string connectionString) => new SqlConnection(connectionString);
+
+        public long Insert(string sql, object parameters, IDbConnection connection)
+        {
+            sql += ";select SCOPE_IDENTITY() id";
+            var results = connection.Query<dynamic>(sql, parameters);
+            dynamic first = results.FirstOrDefault();
+            if (first == null)
+            {
+                return 0;
+            }
+            var id = first.id;
+            return id == null ? 0 : (long)id;
+        }
+    }
+}
