879e9b7910ea84c9e67205d97b033a40bc6098fe
billshu <bill.shu@gravitysupplychain.com>
2019-08-22 15:46:30 +0800

    Add comment

--- src/Dakata/AutoIncrementAttribute.cs
+++ src/Dakata/AutoIncrementAttribute.cs
@@ -3,7 +3,8 @@
 namespace Dakata
 {
     /// <summary>
-    /// Indicates that the column is auto incremented when inserting. Commonly used on Identity column
+    /// Indicates that the column is auto incremented when inserting. Commonly used on Identity column in SQL Server or 
+    /// column with Sequence as default value. For MySQL it's AUTO INCREMENT columns
     /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class AutoIncrementAttribute : Attribute
